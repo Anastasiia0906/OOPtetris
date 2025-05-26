98f34fdc7bae9029ab79325f8ab0553f62fd5480
Anastasiia0906 <kostenko.anastasiia1123@vu.cdu.edu.ua>
2025-05-26 17:13:18 +0300

    App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,17 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
+using Tetris;
 
-namespace курсова1
+namespace Tetris
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        public static GameSettings GlobalSettings { get; private set; }
+        public static bool IsDarkTheme { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Загружаем глобальные настройки
+            GlobalSettings = GameSettings.Load();
+            IsDarkTheme = GlobalSettings.IsDarkTheme;
+            // Применяем тему ко всем окнам (включая только-только загружаемое)
+            ApplyTheme(IsDarkTheme);
+        }
+
+        public static void ApplyTheme(bool isDark)
+        {
+            IsDarkTheme = isDark;
+            GlobalSettings.IsDarkTheme = isDark;
+            GlobalSettings.Save();
+
+            // Обновляем все открытые окна
+            if (Current is App app)
+            {
+                foreach (Window w in app.Windows)
+                {
+                    if (w is IThemableWindow tw)
+                        tw.ApplyTheme(isDark);
+                }
+            }
+        }
     }
 }
